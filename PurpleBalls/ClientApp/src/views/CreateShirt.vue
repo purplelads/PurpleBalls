0bcc1cbd495cb9ee370616fafc8136b439a9eb07
Dale Causier <dale.causier@riveragency.com>
2019-08-08 16:03:24 +0100

    Beginnings of Create a Shirt Customization

--- PurpleBalls/ClientApp/src/views/CreateShirt.vue
+++ PurpleBalls/ClientApp/src/views/CreateShirt.vue
@@ -9,14 +9,48 @@
           :return-object="true"
           v-model="selectedPlayer"
           solo
-        >
-        
+        >        
         </v-select>
-        <PlayerShirt v-if="selectedPlayerShirt != null" :shirtInfo='selectedPlayerShirt'></PlayerShirt>
+        <div>
+        <v-form id="create-player-form">
+          <v-card-title>
+            <span class="headline">Customize Your Shirt</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm6 md6>
+                  <v-text-field label="Shirt Name*" v-model="selectedPlayerShirt.shirtName"></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md6>
+                  <v-text-field label="Shirt Number*" v-model="selectedPlayerShirt.shirtNumber"></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+        </v-form>
+
+        <PlayerShirt id="player-shirt" v-if="selectedPlayerShirt != null" :shirtInfo='selectedPlayerShirt'></PlayerShirt>
+      </div>
       </v-layout>
     </v-slide-y-transition>    
   </v-container>
 </template>
+
+<style>
+#player-shirt{
+  width: 50%;
+  float:left;
+  padding: 0px 20px;
+}
+#create-player-form{
+  width: 50%;
+  float: left;
+  background-color: #eaeaea;
+}
+</style>
+
 
 <script lang='ts'>
 import { Component, Vue, Watch } from 'vue-property-decorator';
@@ -58,6 +92,9 @@ export default class CreateShirtView extends Vue {
       return _.find(self.playerShirts, (s: Player) => {
         return s.playerId === self.selectedPlayer.playerId;
       });
+    }
+    else{
+      return new PlayerShirtModel(null, "Example", 0, 1, "#000000", "#000000", "#FFFFFF", null)
     }
   }
 
